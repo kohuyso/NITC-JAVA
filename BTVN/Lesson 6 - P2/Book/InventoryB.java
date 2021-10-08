82f8dc059366f63f261b0fc981e9ebf4c5358f63
kohuyso <90167719+kohuyso@users.noreply.github.com>
2021-10-08 10:29:47 +0700

    Add files via upload

---
+++ BTVN/Lesson 6 - P2/Book/InventoryB.java
@@ -0,0 +1,47 @@
+package MB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InventoryB {
+	static private List<Book> inventory = new ArrayList<Book>();
+
+	private InventoryB() {
+
+	}
+
+	static public List<Book> getInstance() {
+		return inventory;
+	}
+
+	static public boolean addToInventory(Book book) {
+		inventory.add(book);
+		return true;
+	}
+	
+	static public boolean addQuantity (int id, int quantity) {
+		if (InventoryB.isValid (id) == false) {
+			return false;
+		}
+		
+		Book temp = InventoryB.getBook(id);
+		int oldQuantity = temp.getQuantity();
+		temp.setQuantity(oldQuantity + quantity);
+		
+		return true;
+	}
+ 
+	static public boolean isValid(int id) {
+		for (Book i : inventory) {
+			if (i.getId() == id) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static Book getBook(int id) {
+		return inventory.get(id - 1);
+	}
+
+}
